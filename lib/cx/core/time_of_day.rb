8249bf3053e6ca94a136e2e3c8cc337ab5ff7c2a
balmoral <colgunn@me.com>
2015-09-18 11:31:25 +1000

    work in progress

--- lib/cx/core/time_of_day.rb
+++ lib/cx/core/time_of_day.rb
@@ -1,10 +1,21 @@
 class TimeOfDay
   # Returns array [hour, minute, second]
   # given an a second of the day.
-  def self.to_hms(second_of_day)
+  def self.hms(second_of_day)
     h = (second_of_day / 3600).to_i
     m = ((second_of_day - (h * 3600)) / 60).to_i
     s = second_of_day % 60
     [h, m, s]
+  end
+
+  def second(*args)
+    if args[0].respond_to?(:hour) # Time or DateTime
+      h = args[0].hour
+      m = args[0].min
+      s = args[0].sec
+    else
+      h, m, s = args
+    end
+    (h.to_i * 3600) + (m.to_i * 60) + s.to_i
   end
 end
