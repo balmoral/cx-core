69633a07dbef5f896aa0b5c9e0012b5e0ce5a3d1
balmoral <colgunn@me.com>
2016-01-20 16:14:31 +1100

    work in progress

--- lib/cx/core/class.rb
+++ lib/cx/core/class.rb
@@ -1,16 +1,17 @@
 class Class
-    # Returns a symbol got from final section
+  # Returns a string got from final section
   # (after the last ::) of the given class name.
   # If no :: found the full name is returned
-  def tag
-    unless @tag
+  def moniker
+    unless @moniker
       n = name
       m = n.match(/::(\w*)$/)
-      @tag = (m ? m[1] : n).to_sym
+      @moniker = m ? m[1] : n
     end
-    @tag
+    @moniker
   end
 
+  # Returns array of classes which inherit from the receiver.
   def descendants
     ObjectSpace.each_object(singleton_class).select { |klass| klass < self }
   end
